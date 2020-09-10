67bb9cf3217c2aa0095fc0ec22d1730a3d54c484
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-09-10 19:39:03 -0300

    Configure environment and add global styles

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container} from './style';
+
+const Header: React.FC = () => {
+  return (
+    <Container>
+      Hello World
+    </Container>
+  )
+}
+
+export default Header;
